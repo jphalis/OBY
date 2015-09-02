cd37ed8d2f7f07f2918a67ee08e429e938ba684c
Brandon Taylor <alsoicode@gmail.com>
2015-09-02 13:07:18 -0400

    Added unit test for DonationForm.

--- donations/tests.py
+++ donations/tests.py
@@ -1,3 +1,27 @@
+from decimal import Decimal
+
 from django.test import TestCase
 
-# Create your tests here.
+from accounts.models import MyUser
+from .forms import DonationForm
+
+
+class DonationFormUnitTest(TestCase):
+
+    def setUp(self):
+        my_user, created = MyUser.objects.get_or_create(
+            username='test',
+            password='pbkdf2_sha256$12000$64NIBRztT1eL$ip9P9F2vYdCvIXMQlDNt/Hm+8grJp6nbvj4GZjRVHxc=',
+            email='test@user.com', is_active=True, full_name='Test User')
+
+        self.user = my_user
+
+    def test_charge_id_persisted(self):
+        form = DonationForm(data={'amount': 10}, charge_id='abc123',
+            user=self.user)
+
+        self.assertTrue(form.is_valid(), "DonationForm should be valid")
+
+        donation = form.save()
+        self.assertEqual(donation.charge_id, 'abc123', "Donation charge_id "
+            "should be: abc123. Instead was: {}".format(donation.charge_id))
