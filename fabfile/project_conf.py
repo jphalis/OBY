378451c28decd8dece7f80cb50a1647be67e1a8a
OBY <jphalisnj@gmail.com>
2015-09-14 10:50:36 -0400

    ec2 instance shrink

--- fabfile/project_conf.py
+++ fabfile/project_conf.py
@@ -84,7 +84,7 @@ ec2_keypair = 'oby_key_pair'
 ec2_secgroups = ['ObySecurityGroup']
 
 # API Name of instance type. http://bit.ly/mkWvpn
-ec2_instancetype = 't2.medium'
+ec2_instancetype = 't2.micro'
 
 # Existing instances - add the public dns of your instances here
 # when you have spawned them
