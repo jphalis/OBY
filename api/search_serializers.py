5247ad0cee046c4555af9cd15e2e09ad2164acb5
OBY <jphalisnj@gmail.com>
2015-12-05 14:52:41 -0500

    update search_serializer to remove url_fields.py and add the url directly in the serializer

--- api/search_serializers.py
+++ api/search_serializers.py
@@ -1,13 +1,19 @@
 from rest_framework import serializers
+from rest_framework.reverse import reverse as api_reverse
 
 from accounts.models import MyUser
-from .url_fields import MyUserUrlField
 
 
 class SearchMyUserSerializer(serializers.HyperlinkedModelSerializer):
-    account_url = MyUserUrlField("user_account_detail_api")
+    account_url = serializers.SerializerMethodField()
 
     class Meta:
         model = MyUser
         fields = ('id', 'account_url', 'username', 'full_name',
                   'profile_picture',)
+
+    def get_account_url(self, obj):
+        request = self.context['request']
+        kwargs = {'username': obj.username}
+        return api_reverse('user_account_detail_api', kwargs=kwargs,
+                           request=request)
