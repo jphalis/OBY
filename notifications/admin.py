9aa01dd5eea83ee28a46a9ac419346f830d3cdc1
OBY <jphalisnj@gmail.com>
2015-12-25 12:09:06 -0500

    update notification admin interface

--- notifications/admin.py
+++ notifications/admin.py
@@ -9,7 +9,8 @@ class NotificationAdmin(admin.ModelAdmin):
     # use if you want to limit which category is shown
     # form = PhotoUploadForm
 
-    list_display = ['id', 'recipient', '__unicode__']
+    list_display = ['id', 'recipient', '__unicode__', 'read']
+    list_filter = ['read']
     readonly_fields = ['created', 'modified']
 
     class Meta:
